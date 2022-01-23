81e4b5c0d1f3e5379a6a4439ffc41a972bea57ec
Raunak9199 <raunakraj94543@gmail.com>
2022-01-24 00:46:01 +0530

    av

--- lib/view/welcome_page/my_home_page.dart
+++ lib/view/welcome_page/my_home_page.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:my_project_getx/view/welcome_page/components/background.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
+      body: BackGround(
+        child: Column(
+          children: [
+            Text(
+              'Welcome to My App',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
